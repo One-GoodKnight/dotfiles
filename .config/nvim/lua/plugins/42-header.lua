8f601fb301ed40668713339960ba7c9355e327a2
One-GoodKnight <giniaux.alexandre@gmail.com>
2026-05-15 06:13:38 +0200

    fix: lsps back

--- .config/nvim/lua/plugins/42-header.lua
+++ .config/nvim/lua/plugins/42-header.lua
@@ -7,6 +7,15 @@ require("42header").setup({
     auto_update = true,						-- Update header when saving.
     user = "aginiaux",						-- Your user.
     mail = "aginiaux@student.42lyon.fr",	-- Your mail.
+  	asciiart = {
+    	"        :::      ::::::::",
+    	"      :+:      :+:    :+:",
+    	"    +:+ +:+         +:+  ",
+    	"  +#+  +:+       +#+     ",
+    	"+#+#+#+#+#+   +#+        ",
+    	"     #+#    #+#          ",
+    	"    ###   ########lyon.fr",
+  },
 })
 
 vim.g.user = "aginiaux"
