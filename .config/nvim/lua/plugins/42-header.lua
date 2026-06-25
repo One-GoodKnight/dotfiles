d0db65ac5a05451001413db41099feb9f684ac70
One-GoodKnight <giniaux.alexandre@gmail.com>
2026-06-25 22:26:37 +0200

    refactor: 42 header to leader + F1

--- .config/nvim/lua/plugins/42-header.lua
+++ .config/nvim/lua/plugins/42-header.lua
@@ -3,7 +3,7 @@ vim.pack.add({"https://github.com/Diogo-ss/42-header.nvim"})
 
 -- Config
 require("42header").setup({
-	default_map = true,						-- Default mapping <F1> in normal mode.
+	default_map = false,					-- Default mapping <F1> in normal mode.
     auto_update = true,						-- Update header when saving.
     user = "aginiaux",						-- Your user.
     mail = "aginiaux@student.42lyon.fr",	-- Your mail.
@@ -20,3 +20,5 @@ require("42header").setup({
 
 vim.g.user = "aginiaux"
 vim.g.mail = "aginiaux@student.42lyon.fr"
+
+vim.keymap.set("n", "<leader><F1>", ":Stdheader<CR>", { silent = true, noremap = true })
